c0b4ba801d994c0eea177213b9a691002a294c3d
srodrigo <sergio.rodrigo.royo@gmail.com>
2016-11-30 18:55:38 +0000

    Refactor: extracted configuration to Contact Form

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {ContactItem, ContactForm } from './Form';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import ContactForm from './ContactForm'
+import './index.css'
 
 const contacts = [
   {key: 1, name: "James Nelson", email: "james@jamesknelson.com", description: 'Big guy'},
@@ -9,25 +9,7 @@ const contacts = [
   {key: 3, name: "Alice", email: "alice.in.wonderland@whatever.com", description: 'Nice person'}
 ]
 
-var newContact = {name: "", email: "", description: ""}
-
-const contactsElements =
-  contacts
-    .filter(function (contact) {
-      return contact.email
-    })
-    .map(function (contact) {
-      return React.createElement(ContactItem, contact)
-    })
-
-
-const rootElement = React.createElement('div', {},
-  React.createElement('h1', {}, 'Contacts'),
-  React.createElement('ul', {}, contactsElements),
-  React.createElement(ContactForm, {contact: newContact})
-)
-
 ReactDOM.render(
-  rootElement,
+  ContactForm(contacts),
   document.getElementById('root')
-);
+)
